6f104687387c06b9a5e1de7776b7f3aea88c72fa
Fabio Lopes <fabiolopes7844@gmail.com>
2024-10-09 16:46:15 -0300

    Add Registro class to Repository/Headers

---
+++ Repository/Registro.cpp
@@ -0,0 +1,15 @@
+#include "Headers/Registro.h"
+
+Registro::Registro(Data* d, bool entrada, bool manual) {
+    this->data = d;
+    this->entrada = entrada;
+    this->manual = manual;
+}
+
+bool Registro::isEntrada() {
+    return this->entrada;
+}
+
+bool Registro::isManual() {
+    return this->manual;
+}
