5b5b83ce928aebabb7691a7606692f66049516e8
Felipe-Augusto31 <felipe_augusto@usp.br>
2024-10-09 18:51:47 -0300

    update

--- Repository/Data.cpp
+++ Repository/Data.cpp
@@ -1,7 +1,9 @@
 #include <ctime>
+#include <iostream>
 //include de cabecalho
 #include "Headers/Data.h"
 
+using namespace std;
 
 Data::Data(int hora, int minuto, int segundo, int dia, int mes, int ano) {
 
@@ -18,7 +20,25 @@ Data::~Data(){}
 
 int Data::diferenca(Data* d) { 
 //retorna a diferenca em segundos entre a data atual e a data recebida
+
+    time_t t;
+    time(&t); //pegar o tempo desde 70 (atual)
+
+    tm *d1 = new tm(); //criei um obj data
+
+    d1->tm_hour = d->hora;
+    d1->tm_min = d->minuto;
+    d1->tm_sec = d->segundo;
+    d1->tm_isdst = 0;
+    d1->tm_mday = d->dia;
+    d1->tm_mon = d->mes;
+    d1->tm_year = d->ano;
+
+    time_t t1 = mktime(d1); //peguei o obj data e fiz virar um horario (convercao)
     
+    int diferenca = (int) difftime(t,t1); //(time_t inicio, time_t fim)
+
+    return diferenca;
 
 
 }
