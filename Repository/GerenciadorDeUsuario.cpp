d58823aa5e1fcbe267daea41cc2a3ed78ad2e08f
Felipe-Augusto31 <felipe_augusto@usp.br>
2024-10-09 19:01:34 -0300

    up

---
+++ Repository/GerenciadorDeUsuario.cpp
@@ -0,0 +1,8 @@
+#include "GerenciadorDeUsuario.h"
+
+    GerenciadorDeUsuario::GerenciadorDeUsuario(int maximo) {
+        this->maximo = maximo;
+
+    }
+    GerenciadorDeUsuario:: ~GerenciadorDeUsuario(){}
+
