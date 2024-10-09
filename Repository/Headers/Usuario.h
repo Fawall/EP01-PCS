efd65d89b6b6b21ed2fd18f6e94eddf6f62389d5
Fabio Lopes <fabiolopes7844@gmail.com>
2024-10-09 18:05:57 -0300

    Add Usuario class implementation

---
+++ Repository/Headers/Usuario.h
@@ -0,0 +1,36 @@
+#ifndef USUARIO_H
+#define USUARIO_H
+
+#include <iostream>
+#include "Data.h"
+#include "Registro.h"
+
+using namespace std;
+
+class Usuario
+{
+protected:
+    int id;
+    int maximo;
+    string nome;
+    Registro **registros;
+    int quantidade;
+
+public:
+    Usuario(int id, string nome, int maximo);
+    virtual ~Usuario();
+
+    string getNome();
+    int getId();
+
+    bool entrar(Data *d);
+    bool sair(Data *d);
+    bool registrarEntradaManual(Data *d);
+    bool registrarSaidaManual(Data *d);
+
+    int getHorasTrabalhadas(int mes, int ano);
+    Registro **getRegistros();
+    int getQuantidade();
+};
+
+#endif
---
+++ Repository/Usuario.cpp
@@ -0,0 +1,49 @@
+#include "Headers/Usuario.h"
+
+Usuario::Usuario(int id, string nome, int maximo) {
+    this->id = id;
+    this->nome = nome;
+    this->maximo = maximo;
+    this->registros = new Registro*[maximo];
+    this->quantidade = 0;
+}   
+
+
+string Usuario::getNome() {
+    return this->nome;
+}
+
+int Usuario::getId() {
+    return this->id;
+}
+
+bool Usuario::entrar(Data *d) {
+    int ultimoRegistroData = registros[quantidade]->getData()->diferenca(registros[quantidade]->getData());
+    bool ultimoRegistroEntrada = registros[quantidade]->isEntrada();
+
+    int diferenca = d->diferenca(d) - ultimoRegistroData ;
+
+    if(ultimoRegistroEntrada != true || diferenca < 0) {
+        registros[quantidade] = new Registro(d, true, false);
+        quantidade++;
+        return true;
+    }
+    return false;
+}
+
+bool Usuario::sair(Data *d){
+    int ultimoRegistroData = registros[quantidade]->getData()->diferenca(registros[quantidade]->getData());
+    bool ultimoRegistroSaida = registros[quantidade]->isEntrada();
+
+    int diferenca = d->diferenca(d) - d->diferenca(registros[quantidade]->getData()) ;
+
+    if(ultimoRegistroSaida == true || diferenca < 0) {
+        if(quantidade > 0)
+            quantidade -= 1;
+        registros[quantidade] = new Registro(d, false, false);
+        return true;
+    }
+    return false;
+}
+
+
