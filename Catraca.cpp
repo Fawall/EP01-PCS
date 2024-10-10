caa67afbd2d7565f766ee3be7aa3a0cf36e24196
Fabio Lopes <fabiolopes7844@gmail.com>
2024-10-10 17:29:42 -0300

    Add Catraca class with methods to handle user entry and exit

---
+++ Catraca.cpp
@@ -0,0 +1,27 @@
+#include "Catraca.h"
+
+
+Catraca::Catraca(GerenciadorDeUsuario* g) {
+    this->g = g;
+}
+
+bool Catraca::entrar(int id, Data* d) {
+    Usuario* usuario = g->getUsuario(id);
+
+    if( usuario != nullptr){
+        usuario->entrar(d);
+        return true;
+    }
+    return false;
+    
+}
+
+bool Catraca::sair(int id, Data* d) {
+    Usuario* usuario = g->getUsuario(id);
+
+    if( usuario != nullptr){
+        usuario->sair(d);
+        return true;
+    }
+    return false;
+}
