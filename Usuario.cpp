95de22bf694250905fab5deb17789b16aae63b7c
Fabio Lopes <fabiolopes7844@gmail.com>
2024-10-11 17:44:35 -0300

    Refactor Usuario class to fix entrance and exit registration logic

--- Usuario.cpp
+++ Usuario.cpp
@@ -1,32 +1,74 @@
 #include "Usuario.h"
 
-Usuario::Usuario(int id, string nome, int maximo) {
+Usuario::Usuario(int id, string nome, int maximo)
+{
     this->id = id;
     this->nome = nome;
     this->maximo = maximo;
-    this->registros = new Registro*[maximo];
-}   
+    this->registros = new Registro *[maximo];
+}
 
-
-string Usuario::getNome() {
+string Usuario::getNome()
+{
     return this->nome;
 }
 
-int Usuario::getId() {
+int Usuario::getId()
+{
     return this->id;
 }
 
-bool Usuario::entrar(Data *d) {
-    if(quantidade >= maximo) {
+// bool Usuario::entrar(Data *d)
+// {
+//     if (quantidade >= maximo)
+//     {
+//         return false;
+//     }
+
+//     if (quantidade == 0 && registros[quantidade] == nullptr)
+//     {
+//         {
+//             registros[quantidade] = new Registro(d, true, false);
+//             quantidade++;
+//             return true;
+//         }
+
+//         // int ultimoRegistroData = registros[quantidade]->getData();
+//         bool ultimoRegistroEntrada = registros[quantidade]->isEntrada();
+//         cout << "Ultimo registro entrada: " << ultimoRegistroEntrada << endl;
+
+//         int diferenca = d->diferenca(d) - d->diferenca(registros[quantidade]->getData());
+//         ;
+
+//         if (ultimoRegistroEntrada != true || diferenca > 0)
+//         {
+//             registros[quantidade] = new Registro(d, true, false);
+//             quantidade++;
+//             return true;
+//         }
+//         return false;
+//     }
+// }
+
+bool Usuario::entrar(Data *d)
+{
+    if (quantidade >= maximo)
+    {
         return false;
     }
 
-    int ultimoRegistroData = registros[quantidade]->getData()->diferenca(registros[quantidade]->getData());
-    bool ultimoRegistroEntrada = registros[quantidade]->isEntrada();
+    if (quantidade == 0)
+    {
+        registros[quantidade] = new Registro(d, true, false);
+        quantidade++;
+        return true;
+    }
 
-    int diferenca = d->diferenca(d) - ultimoRegistroData ;
+    bool ultimoRegistroEntrada = registros[quantidade - 1]->isEntrada();
+    int diferenca = d->diferenca(d) - d->diferenca(registros[quantidade - 1]->getData());
 
-    if(ultimoRegistroEntrada != true || diferenca < 0) {
+    if (ultimoRegistroEntrada == false && diferenca > 0)
+    {
         registros[quantidade] = new Registro(d, true, false);
         quantidade++;
         return true;
@@ -34,28 +76,37 @@ bool Usuario::entrar(Data *d) {
     return false;
 }
 
-bool Usuario::sair(Data *d){
-    if(quantidade >= maximo) {
+bool Usuario::sair(Data *d)
+{
+    if (quantidade >= maximo)
+    {
         return false;
     }
 
+    if (quantidade == 0)
+    {
+        registros[quantidade] = new Registro(d, false, false);
+        quantidade ++;
+        return true;
+    }
 
-    int ultimoRegistroData = registros[quantidade]->getData()->diferenca(registros[quantidade]->getData());
-    bool ultimoRegistroSaida = registros[quantidade]->isEntrada();
+    // int ultimoRegistroData = registros[quantidade]->getData()->diferenca(registros[quantidade]->getData());
+    bool ultimoRegistroSaida = registros[quantidade-1]->isEntrada();
 
-    int diferenca = d->diferenca(d) - d->diferenca(registros[quantidade]->getData()) ;
+    int diferenca = d->diferenca(d) - d->diferenca(registros[quantidade-1]->getData());
 
-    if(ultimoRegistroSaida == true || diferenca > 0) {
-        if(quantidade > 0)
-            quantidade -= 1;
+    if (ultimoRegistroSaida == false || diferenca < 0)
+    {
         registros[quantidade] = new Registro(d, false, false);
         return true;
     }
     return false;
 }
 
-bool Usuario::registrarEntradaManual(Data *d) {
-    if(quantidade >= maximo) {
+bool Usuario::registrarEntradaManual(Data *d)
+{
+    if (quantidade >= maximo)
+    {
         return false;
     }
 
@@ -64,7 +115,8 @@ bool Usuario::registrarEntradaManual(Data *d) {
 
     int diferenca = d->diferenca(d) - ultimoRegistroData;
 
-    if(ultimoRegistroEntrada != true || diferenca < 0) {
+    if (ultimoRegistroEntrada != true || diferenca < 0)
+    {
         registros[quantidade] = new Registro(d, true, true);
         quantidade++;
         return true;
@@ -72,8 +124,10 @@ bool Usuario::registrarEntradaManual(Data *d) {
     return false;
 }
 
-bool Usuario::registrarSaidaManual(Data *d) {
-    if(quantidade >= maximo) {
+bool Usuario::registrarSaidaManual(Data *d)
+{
+    if (quantidade >= maximo)
+    {
         return false;
     }
 
@@ -82,8 +136,9 @@ bool Usuario::registrarSaidaManual(Data *d) {
 
     int diferenca = d->diferenca(d) - d->diferenca(registros[quantidade]->getData());
 
-    if(ultimoRegistroSaida == true || diferenca > 0) {
-        if(quantidade > 0)
+    if (ultimoRegistroSaida == true || diferenca > 0)
+    {
+        if (quantidade > 0)
             quantidade -= 1;
         registros[quantidade] = new Registro(d, false, true);
         return true;
@@ -91,17 +146,20 @@ bool Usuario::registrarSaidaManual(Data *d) {
     return false;
 }
 
-Registro* *Usuario::getRegistros() {
-    return  this->registros;
+Registro **Usuario::getRegistros()
+{
+    return this->registros;
 }
 
-int Usuario::getQuantidade() {
+int Usuario::getQuantidade()
+{
     return this->quantidade;
 }
 
-
-Usuario::~Usuario(){    
-    for(int i = quantidade; i >= 0; i--) {
+Usuario::~Usuario()
+{
+    for (int i = quantidade; i >= 0; i--)
+    {
         delete registros[i];
     }
     delete registros;
