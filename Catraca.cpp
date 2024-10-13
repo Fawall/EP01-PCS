10be5b13b578125ca3ac268ee7524fe83c998113
Fabio Lopes <fabiolopes7844@gmail.com>
2024-10-13 11:50:45 -0300

    Refactor Catraca class to add logging for user exits

--- Catraca.cpp
+++ Catraca.cpp
@@ -21,10 +21,13 @@ bool Catraca::entrar(int id, Data* d) {
 bool Catraca::sair(int id, Data* d) {
     Usuario* usuario = g->getUsuario(id);
 
-    if( usuario != nullptr){
+    if( usuario != false){
         usuario->sair(d);
+
+        cout << "[Saida] Catraca 1" << " abriu: id " << id << endl;
         return true;
     }
+    cout << "NAO SAIU";
     return false;
 }
 
