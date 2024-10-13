56b1e1d5b37c7a18adc05e8966d1a9918c242ee3
Fabio Lopes <fabiolopes7844@gmail.com>
2024-10-13 11:51:07 -0300

    Refactor menu function to improve user interaction and fix variable assignment

--- menu.cpp
+++ menu.cpp
@@ -9,9 +9,21 @@ using namespace std;
 
 void menu()
 {
+    Data *d;
+    GerenciadorDeUsuario *u = new GerenciadorDeUsuario(10);
+    Catraca *catracaEntrar = new Catraca(u);
+    Catraca *catracaSair = new Catraca(u);
+
+    int id = 0;
+    int hora = 0; 
+    int minuto = 0;
+    int segundo = 0;
+    int dia = 0;
+    int mes = 0;
+    int ano = 0;
 
     int escolha;
-    while (escolha != 0)
+    do
     {
         cout << "Acesso ao predio" << endl;
         cout << "1) Entrada" << endl;
@@ -21,43 +33,85 @@ void menu()
         cout << "5) Relatorio" << endl;
         cout << "0) Sair" << endl;
 
-        cout << "Escolha uma opcao:";
-        cin >> escolha;
+        cout << "Escolha uma opcao: ";
+        // cin >> escolha;
+        escolha = 1;
 
-        switch(escolha){
-            case 1:
-                int id = 0;
-                int hora = 0;
-                int minuto = 0;
-                int segundo = 0;
-                int dia = 0;
-                int mes = 0;
-                int ano = 0;
+        switch (escolha)
+        {
+        case 1:
+            id = 1;
+            hora = 2;
+            minuto = 0;
+            segundo = 30;
+            dia = 11;
+            mes = 9;
+            ano = 2004;
 
-                Data* d;
-                cout << "Catraca: 0" << endl;
-                cout << "Id: " << endl;
-                cin >> id;
-                cout << "Hora: ";
-                cin >> hora;
-                cout << "Minuto: " ;
-                cin >> minuto;
-                cout << "Segundo: ";
-                cin >> segundo;
-                cout << "Dia: ";
-                cin >> dia;
-                cout << "Mes: ";
-                cin >> mes;
-                cout << "Ano: ";
-                cin >> ano;
+            // cout << "Catraca: 0" << endl;
+            // cout << "Id: ";
+            // cin >> id;
+            // cout << "Hora: ";
+            // cin >> hora;
+            // cout << "Minuto: " ;
+            // cin >> minuto;
+            // cout << "Segundo: ";
+            // cin >> segundo;
+            // cout << "Dia: ";
+            // cin >> dia;
+            // cout << "Mes: ";
+            // cin >> mes;
+            // cout << "Ano: ";
+            // cin >> ano;
 
-                d = new Data(hora, minuto, segundo, dia, mes, ano);
-                GerenciadorDeUsuario* u;
-                Catraca* c = new Catraca(u);
-                c->entrar(id, d);
-                break;
+            d = new Data(hora, minuto, segundo, dia, mes, ano);
+
+            catracaEntrar->entrar(id, d);
+            cout << endl;
+
+            break;
+        case 2:
+            id = 1;
+            hora = 1;
+            minuto = 0;
+            segundo = 30;
+            dia = 11;
+            mes = 9;
+            ano = 2004;
+
+            // cout << "Catraca: 0" << endl;
+            // cout << "Id: ";
+            // cin >> id;
+            // cout << "Hora: ";
+            // cin >> hora;
+            // cout << "Minuto: " ;
+            // cin >> minuto;
+            // cout << "Segundo: ";
+            // cin >> segundo;
+            // cout << "Dia: ";
+            // cin >> dia;
+            // cout << "Mes: ";
+            // cin >> mes;
+            // cout << "Ano: ";
+            // cin >> ano;
+
+            d = new Data(hora, minuto, segundo, dia, mes, ano);
+
+            catracaSair->sair(id, d);
+            cout << endl;
+
+            break;
+
+        case 4:
+            id = 1;
+            string nome = "Maria";
+
+            Usuario *user = new Usuario(id, nome, 10);
+
+            u->adicionar(user);
+            cout << endl;
+            break;
         }
 
-    }
-
+    } while (escolha != 0);
 }
