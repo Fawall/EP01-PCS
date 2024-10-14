b1a9e86d1848bd93b1809d683c177ea22447a332
Fabio Lopes <fabiolopes7844@gmail.com>
2024-10-14 11:33:16 -0300

    Refactor Data class to remove commented out code

--- Data.cpp
+++ Data.cpp
@@ -18,32 +18,6 @@ this->ano = ano;
 
 Data::~Data(){}
 
-// int Data::diferenca(Data* d) { 
-// //retorna a diferenca em segundos entre a data atual e a data recebida
-
-//     time_t t;
-//     time(&t); //pegar o tempo desde 70 (atual)
-
-//     tm *d1 = new tm(); //criei um obj data
-
-//     d1->tm_hour = d->hora;
-//     d1->tm_min = d->minuto;
-//     d1->tm_sec = d->segundo;
-//     d1->tm_isdst = 0;
-//     d1->tm_mday = d->dia;
-//     d1->tm_mon = d->mes;
-//     d1->tm_year = d->ano;
-
-//     time_t t1 = mktime(d1); //peguei o obj data e fiz virar um horario (convercao)
-    
-//     int diferenca = (int) difftime(t,t1); //(time_t inicio, time_t fim)
-
-//     cout << "DIFERENCA: "<< diferenca <<endl;
-
-
-//     return diferenca;
-
-// }
 
 int Data::diferenca(Data* d) {
     tm d1 = {};
