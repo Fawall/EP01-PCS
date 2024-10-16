aa1aa12aee4d691d7d20d03190dd986d75fd5f85
Fabio Lopes <fabiolopes7844@gmail.com>
2024-10-16 17:52:55 -0300

    Refactor Catraca::entrar method to remove redundant code and improve logic

--- Catraca.cpp
+++ Catraca.cpp
@@ -7,18 +7,10 @@ Catraca::Catraca(GerenciadorDeUsuario* g) {
 }
 
 bool Catraca::entrar(int id, Data* d) {
-    // int id = g->getUsuario(id)->getId();
-    // string nome = g->getUsuario(id)->getNome();
-
-    // Usuario* usuario = new Usuario(id, nome, 10);
-
-    // bool verificaUsuario = usuario->entrar(d);
-
 
     bool verifica = g->getUsuario(id)->entrar(d);
 
     if(g->getUsuario(id) != nullptr && verifica != false){
-        g->getUsuario(id)->entrar(d); 
         return true;
     }
     return false;
